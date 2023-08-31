6a54727fd96652875f3b2290e81b88a1d30dfc71
aytuns <ayotunde.jerry@gmail.com>
2023-08-31 05:32:02 +0100

    creating a blog in django

---
+++ blog/myblog/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+@admin.register(Post)
+class Postadmin(admin.ModelAdmin):
+    list_display = ('title', 'body', 'date_created', 'updated_on', 'author','category')
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Author)
+class Author(admin.ModelAdmin):
+    list_display = ('display_name','user','profile')
